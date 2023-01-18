56ec0b6f049e1c1bee8865100819692c9575e497
Schnitzel <78065512+Like4Schnitzel@users.noreply.github.com>
2023-01-18 23:14:05 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,32 @@
-import java.io.*;
-import java.net.InetAddress;
+import javax.swing.*;
 
 public class Main {
-    public static boolean ffmpegInstalled() {
-        Runtime rt = Runtime.getRuntime();
-        String[] command = {"ffmpeg", "-version"};
-        try {
-            rt.exec(command);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    public static boolean ytdlpInstalled() {
-        Runtime rt = Runtime.getRuntime();
-        String[] command = {"yt-dlp", "--version"};
-        try {
-            Process run = rt.exec(command);
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(run.getInputStream()));
-            String s = stdInput.readLine();
-            Integer.parseInt(s.substring(0, 4));
-            Integer.parseInt(s.substring(5, 7));
-            Integer.parseInt(s.substring(8, 10));
-            if (!(s.charAt(4) == '.' && s.charAt(7) == '.')) {
-                throw new IOException();
-            }
-            return true;
-        } catch (IOException | NullPointerException | NumberFormatException e) {
-            return false;
-        }
-    }
-
     public static void main(String[] args) {
-        //check if an internet connection is established
+        DependencyChecker checker = new DependencyChecker();
+
+        if (!checker.readConfig() || !checker.checkDependencies()) {
+            return;
+        }
+
+        GUI mainWindow;
+        Loader loader;
         try {
-            InetAddress.getByName("www.youtube.com").isReachable(3);
-        } catch (IOException error) {
-            //error message if there is no internet connection
-            ErrorMessage noInternet = new ErrorMessage("Please establish an internet connection.");
-            noInternet.close.setText("Ok");
+            mainWindow = new GUI(checker);
+            loader = new Loader(mainWindow, checker, Integer.parseInt(checker.configValues.get("delay-seconds")));
+        } catch (NumberFormatException e) {
+            ErrorMessage confValues = new ErrorMessage("<html>Config values for window layout <br> must only be integers.", 300, 150);
+            confValues.errorMessage.setBounds(confValues.errorMessage.getX(), confValues.errorMessage.getY(), confValues.errorMessage.getWidth(), 30);
             return;
         }
 
-        if (!ffmpegInstalled()) {
-            new ErrorMessage("ffmpeg could not be found.");
-            return;
-        }
-
-        if (!ytdlpInstalled()) {
-            new ErrorMessage("yt-dlp could not be found.");
-            return;
-        }
-
-        GUI mainWindow = new GUI(500, 300, 10, 20, 25);
-        Loader loader = new Loader(mainWindow, 1);
-
+        loader.setKeyListener();
         loader.setLinkListener();
-
         loader.setArtistListeners();
-
         loader.setAlbumListeners();
-
         loader.setTitleListeners();
-
         loader.setPlaylistListeners();
-
+        loader.setSectionListeners();
         loader.initOutputDirectoryField();
-
         loader.setDownloadListener();
     }
 }
