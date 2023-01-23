d46097a6f49a1b9ea2dddf3889be6395d04a9799
Schnitzel <78065512+Like4Schnitzel@users.noreply.github.com>
2023-01-23 10:40:50 +0100

    Fixed path decoding & added replace-chars var

--- src/DependencyChecker.java
+++ src/DependencyChecker.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.InetAddress;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Scanner;
@@ -9,10 +11,20 @@ public class DependencyChecker {
     public Dictionary<String, String> configValues;
 
     public DependencyChecker() {
-        currentDirectory = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).toString();
-        currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf('/')).replaceAll("%20", " ");
+        try {
+            currentDirectory = filepathFormatter(URLDecoder.decode(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).toString(), StandardCharsets.UTF_8.name()));
+        } catch (UnsupportedEncodingException e) {/*can't happen*/}
+        currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf('/'));
         System.out.println("Current directory is " + currentDirectory);
         configValues = new Hashtable<>();
+    }
+
+    private String filepathFormatter(String originalPath) {
+        if (originalPath.charAt(1) == ':' && originalPath.charAt(2) == '\\') {
+            return originalPath.substring(2).replace('\\', '/');
+        }
+
+        return originalPath;
     }
 
     public boolean checkDependencies() {
@@ -39,12 +51,13 @@ public class DependencyChecker {
         try {
             configValues.put("ffmpeg-path", "ffmpeg");
             configValues.put("yt-dlp-path", "yt-dlp");
-            configValues.put("frame-width", "600");
+            configValues.put("delay-seconds", "1");
+            configValues.put("replace-chars", "<>:\"/\\|?*");
+            configValues.put("frame-width", "620");
             configValues.put("frame-height", "350");
             configValues.put("left-bound", "10");
             configValues.put("upper-bound", "20");
             configValues.put("line-distance", "25");
-            configValues.put("delay-seconds", "1");
             Scanner configReader = new Scanner(new File(currentDirectory + "/config.txt"));
             String configString = "";
             for (int i = 0; configReader.hasNextLine(); i++) {
@@ -58,8 +71,16 @@ public class DependencyChecker {
                     return false;
                 }
 
-                if (substrings[1].charAt(0) == '.') {
-                    configValues.put(substrings[0], currentDirectory + substrings[1].substring(1));
+                if (substrings[0].contains("path")) {
+                    try {
+                        if (substrings[1].charAt(0) == '.') {
+                            configValues.put(substrings[0], filepathFormatter(currentDirectory + substrings[1].substring(1)));
+                        } else {
+                            configValues.put(substrings[0], filepathFormatter(substrings[1]));
+                        }
+                    } catch (StringIndexOutOfBoundsException e) {
+                        configValues.put(substrings[0], substrings[1]);
+                    }
                 } else {
                     configValues.put(substrings[0], substrings[1]);
                 }
